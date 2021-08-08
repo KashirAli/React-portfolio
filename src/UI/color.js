7a6544aed10fabc528cbf2579991fa503c4ed51e
unknown <sheikh.kashir.sk@gmail.com>
2021-08-09 02:10:00 +0500

    Updates

--- src/UI/color.js
+++
@@ -1,9 +0,0 @@
-export const color = {
-    bgWarning:'#FFC107',
-    bgDanger:'#DC3545',
-    bgSuccess:'#198754',
-    bgInfo:'#0DCAF0',
-    textPrimary:'#fafafc',
-    textSecondary:'#8c8c8e'
-
-}
